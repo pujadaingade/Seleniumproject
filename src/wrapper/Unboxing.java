cbfe598d154d894306f331c7a800d8d26e2f2e6a
puja <daingade.puja3@gmail.com>
2019-04-04 10:48:41 +0530

    commited successfully

---
+++ src/wrapper/Unboxing.java
@@ -0,0 +1,11 @@
+package wrapper;
+
+public class Unboxing {
+public static void main(String[] args) {
+	
+	//Wrapper to Primitive
+	Integer i=new Integer(4);
+	int a=i.intValue();
+	System.out.println(a);
+}
+}
---
+++ src/wrapper/Wrapperclass.java
@@ -0,0 +1,11 @@
+package wrapper;
+
+public class Wrapperclass {
+public static void main(String[] args) {
+	
+	//autoboxing(Primitive to Wrapper
+	int a=10;
+	Integer b=a;
+	System.out.println(b);
+}
+}
